68fea1a136a1e2c2d48066b3b8465c14dac2504a
andreas893 <eaa24andrh@students.eaaa.dk>
2026-04-21 13:42:00 +0200

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-black text-white'>
+        Tailwind virker
+      </div>
+    </>
+  )
+}
+
+export default App
